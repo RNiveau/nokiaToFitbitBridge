ced9d918d0ef14cb4885021c8b78cb931b19a18a
Romain Niveau <romain.niveau@gmail.com>
2017-07-19 09:20:58 +0200

    Test clients on app

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict';
 
-var nokia_client = require('./nokia_client');
+var nokia_client = require('./nokia_client'),
+    fitbit_client = require('./fitbit_client'),
+    date_format = require('date-format');
 
 var app = async function () {
 
-  let data = await nokia_client.get_data();
-
+  let data = await nokia_client.get_last_weight();
   console.log(data);
-}
+  data = await fitbit_client.get_weight_to_date(date_format.asString('yyyy-MM-dd'));
+  console.log(data);
+};
 
 app();
