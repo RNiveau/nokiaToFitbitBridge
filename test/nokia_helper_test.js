68763c62eb867247520385c59c199dca4fbde88e
Romain Niveau <romain.niveau@gmail.com>
2017-07-21 09:31:28 +0200

    Add unit test on nokia_helper

--- test/nokia_helper_test.js
+++ test/nokia_helper_test.js
@@ -2,6 +2,7 @@
 
 var assert = require('assert'),
     nokia_helper = require('../src/nokia_helper'),
+    _ = require('lodash'),
     data = require('./data/nokia.json');
 
 describe('Nokia helper', function () {
@@ -11,12 +12,19 @@ describe('Nokia helper', function () {
       assert.equal(nokia_helper.get_weight(data), 57.364);
     });
 
-    it('should return 0 when no measure is present', function () {
-      data.body.measuregrps = [];
-      assert.equal(nokia_helper.get_weight(data), 0);
+    it('should return 57364 from nokia json with 0 as unit', function () {
+      let copy_data = _.cloneDeep(data);
+      copy_data.body.measuregrps[0].measures[0].unit = 0;
+      assert.equal(nokia_helper.get_weight(copy_data), 57364);
+    });
 
-      data.body.measuregrps = [{measures: []}];
-      assert.equal(nokia_helper.get_weight(data), 0);
+    it('should return 0 when no measure is present', function () {
+      let copy_data = _.cloneDeep(data);
+      copy_data.body.measuregrps = [];
+      assert.equal(nokia_helper.get_weight(copy_data), 0);
+
+      copy_data.body.measuregrps = [{measures: []}];
+      assert.equal(nokia_helper.get_weight(copy_data), 0);
     });
 
   });
