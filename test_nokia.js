31f2cd82267654907ea81adcdec4e6f374e19d0d
Romain Niveau <romain.niveau@gmail.com>
2018-11-03 20:00:33 +0100

    Add test to run nokia client

---
+++ test_nokia.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var nokia_client = require('./src/nokia_client'),
+    logger = require('./src/logger');
+
+var app = async function () {
+    let nokia_data = await nokia_client.get_last_weight();
+    logger.debug(nokia_data);
+};
+
+try {
+  logger.info("Start test nokia");
+  app()
+      .catch(error => logger.error(error));
+} catch (exception) {
+  logger.error(exception);
+}
